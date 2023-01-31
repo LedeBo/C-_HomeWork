22948bb5d33d1628767cc5003a9d98067605b3fa
Elena Bogdanova <denissova@bk.ru>
2023-01-31 13:10:04 +0300

    add solution for the Task1

---
+++ Semitar1_Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+System.Console.WriteLine("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+System.Console.WriteLine("a > b, a = max: " + a);
+}
+else if (a<b)
+{
+System.Console.WriteLine("a < b, b = max: " + b);
+}
+else
+System.Console.WriteLine("a = b, значения равны: " + a + " = "  + b);
