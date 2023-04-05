f7cd14b89aea8b63d2de1bb4c41758812982aaf1
Elena Bogdanova <denissova@bk.ru>
2023-04-05 15:57:43 +0300

    added tasks to seminar 4

---
+++ Sem4Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+int Size(string message)
+{
+    System.Console.Write(message);
+    string ReadInput = System.Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+
+int[] GenerateArray(int size, int LeftRange, int RightRange)
+{
+    int[] array = new int[size];
+    var rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rand.Next(LeftRange, RightRange + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine("[" + string.Join(", ", array) + "]");
+}
+
+int size = Size("Задайте длинну массива: ");
+int LeftRange = Size("Начальное значение массива: ");
+int RightRange = Size("Конечное значение массива: ");
+int[] array = GenerateArray(8, 1, 33);
+
+PrintArray(array);
