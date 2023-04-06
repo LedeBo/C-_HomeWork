ffbf886fca1f8eee94d898b9dd43947bd80d4d6f
Elena Bogdanova <denissova@bk.ru>
2023-04-06 14:42:50 +0300

    added tasks for the seminar 6

---
+++ Sem6Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Введите числа: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int Count(int[] array)
+{
+    int C = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            C++;
+        }
+
+    }
+    return C;
+}
+
+System.Console.WriteLine(Count(array));
