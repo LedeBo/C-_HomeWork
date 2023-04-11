004383a71ec891e1544a4248c2decb1d494be302
Elena Bogdanova <denissova@bk.ru>
2023-04-11 10:50:10 +0300

    added solution to the tasks for the Seminar 7

---
+++ Sem7Task2/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GenerateMatrix(int rows, int cols)
+{
+    Random rand = new Random();
+    int[,] matrix = new int[rows, cols];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(0, 11);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write(matrix[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int CheckIndex(int[,] matrix)
+{
+metka:
+    int n = ReadInt("Введите номер строки матрицы: ");
+    int m = ReadInt("Введите номер столбца матрицы: ");
+
+    if (n > matrix.GetLength(0) || m > matrix.GetLength(1))
+    {
+
+        System.Console.WriteLine("Элемента матрицы с такими индексами не существует");
+        goto metka;
+
+    }
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+
+            if (n <= matrix.GetLength(0) || m <= matrix.GetLength(1))
+            {
+
+                System.Console.WriteLine($"Значение элемента с индексами: {n} , {m} равно: {matrix[n, m]} ");
+
+            }
+            break;
+        }
+        break;
+        Console.WriteLine();
+
+    }
+    return matrix[n, m];
+}
+
+int rows = ReadInt("Введите количество строк матрицы: ");
+int cols = ReadInt("Введите количество столбцов матрицы: ");
+
+
+System.Console.WriteLine();
+
+var myMatrix = GenerateMatrix(rows, cols);
+
+PrintMatrix(myMatrix);
+
+CheckIndex(myMatrix);
+
---
+++ Sem7Task3/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int rows = ReadInt("Введите количество строк: ");
+int colums = ReadInt("Введите количество столбцов: ");
+int[,] matrix = new int[rows, colums];
+
+FillArray(matrix);
+PrintArray(matrix);
+
+double sum = 0;
+double result = 0;
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+result = sum / matrix.GetLength(1);
+result = Math.Round(result, 2);
+Console.Write(result + "; ");
+result = 0;
+sum = 0;
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 9);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
